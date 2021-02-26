fee9dcc905eb6e40602cd74ca1fb0615f871feef
Abdelmonem Mohamed <Abdelmonem@Abdelmonems-MacBook-Pro.local>
2021-02-26 23:17:14 +0300

    modifications

--- dataset.py
+++ dataset.py
@@ -1,27 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from random import randint
-import math
 
 
-n_data_points = 200
+inner_circles_points = 200
+outer_circles_points = 50
 
-rad = 0.1
-rad2 = 0.2
-rad3 = 0.3
-thk = 0.1
-sep = 0.1
+circle_no2_rad = 0.1
+circle_no3_rad = 0.2
+circle_no4_rad = 0.3
+
+circle_thickness = 0.1
 
 
+# draw the first circle
+r1 = np.random.rand(outer_circles_points)*circle_thickness
+a1 = np.random.rand(outer_circles_points)*np.pi
+r2 = np.random.rand(outer_circles_points)*circle_thickness
+a2 = np.random.rand(outer_circles_points)*np.pi+np.pi
 
-# We use random radius in the interval [rad, rad+thk]
-#  and random angles from 0 to pi radians.
-r1 = np.random.rand(50)*thk
-a1 = np.random.rand(50)*np.pi
-r2 = np.random.rand(50)*thk
-a2 = np.random.rand(50)*np.pi+np.pi
-
-# In order to plot it we convert it to cartesian:
 p1 = np.array((r1*np.cos(a1), r1*np.sin(a1)))
 p2 = np.array((r2*np.cos(a2), r2*np.sin(a2)))
 
@@ -30,16 +27,16 @@ x2, y2 = (p2[0], p2[1])
 
 x1 = np.concatenate((x1, x2))
 y1 = np.concatenate((y1, y2))
-c1 = np.zeros(50*2)
-# We use random radius in the interval [rad, rad+thk]
-#  and random angles from 0 to pi radians.
-r3 = np.random.rand(n_data_points)*thk+rad
-a3 = np.random.rand(n_data_points)*np.pi
+c1 = np.zeros(outer_circles_points*2)
 
-r4 = np.random.rand(n_data_points)*thk+rad
-a4 = np.random.rand(n_data_points)*np.pi+np.pi
 
-# In order to plot it we convert it to cartesian:
+# draw the second circle
+r3 = np.random.rand(inner_circles_points)*circle_thickness+circle_no2_rad
+a3 = np.random.rand(inner_circles_points)*np.pi
+
+r4 = np.random.rand(inner_circles_points)*circle_thickness+circle_no2_rad
+a4 = np.random.rand(inner_circles_points)*np.pi+np.pi
+
 p3 = np.array((r3*np.cos(a3), r3*np.sin(a3)))
 p4 = np.array((r4*np.cos(a4), r4*np.sin(a4)))
 
@@ -48,17 +45,16 @@ x4, y4 = (p4[0], p4[1])
 
 x3 = np.concatenate((x3, x4))
 y3 = np.concatenate((y3, y4))
-c3 = np.ones(n_data_points*2)
+c3 = np.ones(inner_circles_points*2)
 
-# We use random radius in the interval [rad, rad+thk]
-#  and random angles from 0 to pi radians.
-r5 = np.random.rand(n_data_points)*thk+rad2
-a5 = np.random.rand(n_data_points)*np.pi
 
-r6 = np.random.rand(n_data_points)*thk+rad2
-a6 = np.random.rand(n_data_points)*np.pi+np.pi
+# draw the third circle
+r5 = np.random.rand(inner_circles_points)*circle_thickness+circle_no3_rad
+a5 = np.random.rand(inner_circles_points)*np.pi
 
-# In order to plot it we convert it to cartesian:
+r6 = np.random.rand(inner_circles_points)*circle_thickness+circle_no3_rad
+a6 = np.random.rand(inner_circles_points)*np.pi+np.pi
+
 p5 = np.array((r5*np.cos(a5), r5*np.sin(a5)))
 p6 = np.array((r6*np.cos(a6), r6*np.sin(a6)))
 
@@ -67,15 +63,15 @@ x6, y6 = (p6[0], p6[1])
 
 x5 = np.concatenate((x5, x6))
 y5 = np.concatenate((y5, y6))
-c5 = np.zeros(n_data_points*2)
+c5 = np.zeros(inner_circles_points*2)
 
-r7 = np.random.rand(50)*thk+rad3
-a7 = np.random.rand(50)*np.pi
+# draw the fourth circle
+r7 = np.random.rand(outer_circles_points)*circle_thickness+circle_no4_rad
+a7 = np.random.rand(outer_circles_points)*np.pi
 
-r8 = np.random.rand(50)*thk+rad3
-a8 = np.random.rand(50)*np.pi+np.pi
+r8 = np.random.rand(outer_circles_points)*circle_thickness+circle_no4_rad
+a8 = np.random.rand(outer_circles_points)*np.pi+np.pi
 
-# In order to plot it we convert it to cartesian:
 p7 = np.array((r7*np.cos(a7), r7*np.sin(a7)))
 p8 = np.array((r8*np.cos(a8), r8*np.sin(a8)))
 
@@ -84,8 +80,10 @@ x8, y8 = (p8[0], p8[1])
 
 x7 = np.concatenate((x7, x8))
 y7 = np.concatenate((y7, y8))
-c7 = np.ones(50*2)
+c7 = np.ones(outer_circles_points*2)
 
+
+# join all points
 x = []
 y = []
 c = []
@@ -109,8 +107,10 @@ points = []
 for i in range(len(x)):
     points.append([x[i],y[i],c[i]])
 
+# plot the data
 plt.scatter(x, y,c=c)
-
-np.savetxt("data.csv", points, delimiter=",")
-
 plt.show()
+
+
+# save the data to the data.csv file
+np.savetxt("data.csv", points, delimiter=",")
--- knn.py
+++ knn.py
@@ -8,29 +8,33 @@ data = np.genfromtxt("data.csv", delimiter=",")
 np.random.shuffle(data)
 
 # calculate the euclidean distance between two points
+
+
 def calculate_euclidean_distance(point1, point2):
     return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
 # get the nearest k neighbors to a point
-def get_k_neighbors(train, test_point, k):
+
+
+def get_nearest_k_neighbors(train, test_point, k):
     distances = []
 
     # get the distance between the test point and the rest of the dataset
-    for train_row in train:
-        dist = calculate_euclidean_distance(test_point, train_row)
-        distances.append([train_row, dist])
+    for i in range(len(train)):
+        dist = calculate_euclidean_distance(test_point, train[i])
+        distances.append([train[i], i, dist])
 
     # sort the distances descending according to the distance
-    distances.sort(key=lambda element: element[1])
+    distances.sort(key=lambda element: element[-1])
 
     # get the first k neighbors
     return distances[:k]
 
 
 # predict the label for a single test point
-def predict(train, test_point, k, plot=False):
+def predict(data, test_point, k, plot=False):
     # get the nearest k neighbors first
-    neighbors = get_k_neighbors(train, test_point, k)
+    neighbors = get_nearest_k_neighbors(data, test_point, k)
     positives = negatives = 0
 
     # loop through all neighbors
@@ -44,7 +48,7 @@ def predict(train, test_point, k, plot=False):
 
     # plot the data
     if plot:
-        plot_data(train, test_point, neighbors[-1][-1])
+        plot_data(data, test_point, neighbors[-1][-1])
 
     # calculate the accuracy of the prediction
     if positives > negatives:
@@ -73,7 +77,7 @@ def predict_all(data, k):
         # if the prediction was incorrect increment the negative
         else:
             negatives += 1
-    # send the total and average accuracy 
+    # send the total and average accuracy
     return positives * 100 / (positives + negatives), total_accuracy / len(data)
 
 
@@ -83,9 +87,9 @@ def plot_data(data, point, radius):
 
     plotter.scatter(data[:, 0], data[:, 1], c=data[:, 2])
 
-    # draw the circle to show the selected pointed, with radius of the maximum distance 
-    ref_region = mpatches.Circle(point[0:2], radius, color="r", alpha=0.3)
-    plotter.add_patch(ref_region)
+    # draw the circle to show the selected pointed, with radius of the maximum distance
+    region = mpatches.Circle(point[0:2], radius, color="r", alpha=0.3)
+    plotter.add_patch(region)
     plotter.set(aspect=1)
 
     plt.scatter(point[0], point[1], c="r")
@@ -103,21 +107,35 @@ def introduce_noise(data, percent):
             data[rand_index][-1] = 1
         else:
             data[rand_index][-1] = 0
+
     return data
 
 # function to remove tomek links
+
+
 def remove_tomek_links(data):
+    tomek_links = np.array([])
     new_data = []
     # loop through the whole data
-    for element in data:
+    for i in range(len(data)):
         # get the nearest neighbor
-        nearest = get_k_neighbors(data, element, 2)[-1]
-        # if they have different labels, ignore them, and don't add them to the new dataset
-        if(element[-1] != nearest[0][-1]):
-            continue
-        # otherwise, add them to the new dataset
-        new_data.append(element)
+        nearest = get_nearest_k_neighbors(data, data[i], 2)[-1]
+        # if they have different labels, add their indices to the tomek links array
+        if(data[i][-1] != nearest[0][-1]):
+            tomek_links = np.append(tomek_links, i)
+            tomek_links = np.append(tomek_links, nearest[1])
 
+    # delete duplicate indices
+    tomek_links = np.unique(tomek_links)
+    # sort
+    tomek_links.sort()
+
+    # add all the data, excpet the ones inside tomek links
+    for i in range(len(data)):
+        if i in tomek_links:
+            continue
+        new_data.append(data[i])
+        
     return np.array(new_data)
 
 
@@ -144,11 +162,11 @@ def main():
     # Test after removing tomek links
     noise_data = introduce_noise(data, 50)
     new_data = remove_tomek_links(noise_data)
-    predicted, actual, accuracy = predict(new_data,new_data[randint(0,len(new_data)-1)],25,True)
-    print(f'Predicted: {predicted}')
-    print(f'Actual: {actual}')
-    print(f'Accuracy: {round(accuracy, 2)}%',end='\n==========================================\n')
-    total_accuracy, average_accuracy = predict_all(new_data, 25)
+    # predicted, actual, accuracy = predict(new_data,new_data[randint(0,len(new_data)-1)],15,True)
+    # print(f'Predicted: {predicted}')
+    # print(f'Actual: {actual}')
+    # print(f'Accuracy: {round(accuracy, 2)}%',end='\n==========================================\n')
+    total_accuracy, average_accuracy = predict_all(new_data, 15)
     print(f'Total Accuracy: {total_accuracy}%')
     print(f'Average Accuracy: {average_accuracy}%')
 
